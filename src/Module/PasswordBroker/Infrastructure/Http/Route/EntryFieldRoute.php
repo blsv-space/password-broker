7668104c5b656bad2349bda0af06d7a7f06ff23d
silex <46520333+silex-rr@users.noreply.github.com>
2026-05-30 07:56:54 +0400

    EntryField Controller & Route

--- src/Module/PasswordBroker/Infrastructure/Http/Route/EntryFieldRoute.php
+++ src/Module/PasswordBroker/Infrastructure/Http/Route/EntryFieldRoute.php
@@ -8,15 +8,16 @@ use App\Module\PasswordBroker\Infrastructure\Http\Controller\EntryController;
 use App\Shared\Infrastructure\Http\Route\AbstractRouterRegistry;
 use Inquisition\Core\Infrastructure\Http\Controller\RestControllerInterface;
 use Inquisition\Core\Infrastructure\Http\Router\RouteGroupInterface;
+use Override;
 
-final readonly class EntryRoute extends AbstractRouterRegistry
+final readonly class EntryFieldRoute extends AbstractRouterRegistry
 {
-    public const string GROUP_NAME = 'entry';
-    public const string PARAM_ENTRY_ID = 'entryId';
+    public const string GROUP_NAME = 'entryField';
+    public const string PARAM_ENTRY_FIELD_ID = 'entryFieldId';
 
     private function __construct() {}
 
-    #[\Override]
+    #[Override]
     public static function register(?RouteGroupInterface $parentRouteGroup = null): void
     {
         $routeGroup = self::inheritGroup(
@@ -25,18 +26,14 @@ final readonly class EntryRoute extends AbstractRouterRegistry
         );
 
         $PARAM_ENTRY_GROUP_ID = EntryGroupRoute::PARAM_ENTRY_GROUP_ID;
-        $PARAM_ENTRY_ID = self::PARAM_ENTRY_ID;
+        $PARAM_ENTRY_FIELD_ID = self::PARAM_ENTRY_FIELD_ID;
         $routeGroup
             ->prefix("/{{$PARAM_ENTRY_GROUP_ID}}/" . self::GROUP_NAME)
             ->get('', EntryController::class, RestControllerInterface::ACTION_INDEX)
-            ->get("/{{$PARAM_ENTRY_ID}}", EntryController::class, RestControllerInterface::ACTION_SHOW)
-            ->get('/search', EntryController::class, EntryController::ACTION_SEARCH)
+            ->get("/{{$PARAM_ENTRY_FIELD_ID}}", EntryController::class, RestControllerInterface::ACTION_SHOW)
             ->post('', EntryController::class, RestControllerInterface::ACTION_STORE)
-            ->put("/{{$PARAM_ENTRY_ID}}", EntryController::class, RestControllerInterface::ACTION_UPDATE)
-            ->delete("/{{$PARAM_ENTRY_ID}}", EntryController::class, RestControllerInterface::ACTION_DESTROY)
-            ->patch("/{{$PARAM_ENTRY_ID}}/move", EntryController::class, EntryController::ACTION_MOVE)
+            ->put("/{{$PARAM_ENTRY_FIELD_ID}}", EntryController::class, RestControllerInterface::ACTION_UPDATE)
+            ->delete("/{{$PARAM_ENTRY_FIELD_ID}}", EntryController::class, RestControllerInterface::ACTION_DESTROY)
         ;
-
-        EntryFieldRoute::register($routeGroup);
     }
 }
